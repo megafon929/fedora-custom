67df2fd6bdd075a2412d579267e7d20d8575502c
Yaroslav Popkov <14361190+megafon929@users.noreply.github.com>
2025-12-29 17:36:13 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 rm -f auto.ks
-rm -r result 
-ksflatten -c base/fedora-gnome-default.ks -o auto.ks -v f43
-livemedia-creator --make-iso --ks auto.ks --no-virt --iso-only --iso-name Fedora-Gnome-Live-x86_64.iso --volid Fedora --releasever 43 --resultdir ./result --nomacboot --project "Fedora custom"
+rm -r result
+echo "Доступны следующие вариант"
+echo "1) fedora-kde-default"
+echo "2) fedora-gnome-default"
+echo "3) fedora-gnome-minimal"
+echo "Выбирете конфиг: "
+read variant
+case $variant in
+    1) echo "Выбран fedora-kde-default" 
+       config=fedora-kde-default
+       name=kde-default
+       ;;
+    2) echo "Выбран fedora-gnome-default"
+       config=fedora-gnome-fefault
+       name=gnome-default
+       ;;
+    3) echo "Выбран fedora-gnome-minimal" 
+       config=fedora-gnome-minimal
+       name=gnome-minimal
+       ;;
+    *) echo "Неверно! Начинайте заново"
+       exit 
+       ;;
+esac
 
+ksflatten -c base/$config.ks -o auto.ks -v f43
+livemedia-creator --make-iso --ks auto.ks --no-virt --iso-only --iso-name $name-live-x86_64.iso --volid Fedora --releasever 43 --resultdir ./result --nomacboot --project "Fedora custom"
