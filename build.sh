3df0b00f2fa0d8bffe8f115d34a626389c4a9cf7
Yaroslav Popkov <14361190+megafon929@users.noreply.github.com>
2025-12-29 19:15:09 +0500

    added nvidia drivers

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-setenforce 0
 rm -f auto.ks
 rm -r result
 echo "Доступны следующие вариант"
 echo "1) fedora-kde-default"
 echo "2) fedora-gnome-default"
 echo "3) fedora-gnome-minimal"
-echo "Выбирете конфиг: "
+echo -n "Выбирете конфиг: "
 read variant
 case $variant in
     1) echo "Выбран fedora-kde-default" 
@@ -14,7 +13,7 @@ case $variant in
        name=kde-default
        ;;
     2) echo "Выбран fedora-gnome-default"
-       config=fedora-gnome-fefault
+       config=fedora-gnome-default
        name=gnome-default
        ;;
     3) echo "Выбран fedora-gnome-minimal" 
@@ -25,6 +24,20 @@ case $variant in
        exit 
        ;;
 esac
-
+echo "Нужны драйвера?"
+echo "1) Да"
+echo "2) Нет"
+echo -n "Выбирете вариант: "
+read variantn
+case $variantn in
+    1) echo "Выбран вариант с nvidia" 
+       nvidia="akmod-nvidia xorg-x11-drv-nvidia-cuda"
+       ;;
+    2) echo "Выбран вариант без nvidia"
+       ;;
+    *) echo "Неверно! Начинайте заново"
+       exit 
+       ;;
+esac
 ksflatten -c base/$config.ks -o auto.ks -v f43
-livemedia-creator --make-iso --ks auto.ks --no-virt --iso-only --iso-name fedora-$name-live-x86_64.iso --volid Fedora --releasever 43 --resultdir ./result --nomacboot --project "fedora-$name-live-43"
+livemedia-creator --make-iso --ks auto.ks --no-virt --iso-only --iso-name $name-live-x86_64.iso --volid Fedora --releasever 43 --resultdir ./result --nomacboot --project "Fedora custom"
