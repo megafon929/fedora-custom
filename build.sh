cd11ea5e2407df02d343dd84dec55dcc0e7bb2cc
Yaroslav Popkov <14361190+megafon929@users.noreply.github.com>
2025-12-29 15:37:45 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 rm -f auto.ks
 rm -r result 
 ksflatten -c fedora-custom.ks -o auto.ks -v f43
-livemedia-creator --make-iso --ks auto.ks --no-virt --iso-only --iso-name Fedora-custom-Live-x86_64.iso --volid Fedora --releasever 43 --resultdir ./result --nomacboot --project "Fedora M929"
+livemedia-creator --make-iso --ks auto.ks --no-virt --iso-only --iso-name Fedora-custom-Live-x86_64.iso --volid Fedora --releasever 43 --resultdir ./result --nomacboot --project "Fedora custom"
 
